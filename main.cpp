6e3de5a1e881b17201fafd7f3d732ef07ba9e8fd
jerronimo <jerronimo@jerronimo-laptop.(none)>
2014-08-06 16:38:28 +0200

    function changed to lambda

--- main.cpp
+++ main.cpp
@@ -12,7 +12,11 @@ int main()
     thread t[thread_num];
     for(int i=0; i<thread_num; ++i)
     {
-        t[i] = thread(call_from_thread);
+        //t[i] = thread(call_from_thread);
+        t[i] = thread([]()
+        {
+            cout << "hello from thread" << endl;
+        });
     }
     for(int i=0; i<thread_num; ++i) { t[i].join(); }
     return 0;
