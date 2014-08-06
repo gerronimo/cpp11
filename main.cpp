d3e56e5988dc62808ca1cda280efd33aa232285a
jerronimo <jerronimo@jerronimo-laptop.(none)>
2014-08-06 18:18:28 +0200

    thFun added

--- main.cpp
+++ main.cpp
@@ -5,18 +5,19 @@
 using namespace std;
 static const int thread_num = 10;
 
+void thFun(int i)
+{
+      cout << "hello from thread " << i << endl;
+}
 int main()
 {
     vector<thread> workers;
     for(int i=0; i<thread_num; ++i)
     {
-        workers.push_back(thread([]()
-        {
-            cout << "hello from thread" << endl;
-        }));
+        workers.push_back(thread(&thFun, i));
+        
         
     }
-    //for(int i=0; i<thread_num; ++i) { workers[i].join(); }
     for_each(workers.begin(), workers.end(),[](thread& th)
     {
         th.join();
