e921f8002120c5d7ddfb70007bec2d737eeeb0a8
jerronimo <jerronimo@jerronimo-laptop.(none)>
2014-08-06 10:22:47 +0200

    thread started

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <thread>
+using namespace std;
 
+void call_from_thread()
+{
+    cout << "hello from thread" << endl;    
+}
 int main()
 {
+    thread t(call_from_thread);
+    t.join();
     return 0;
 }
